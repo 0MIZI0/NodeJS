5a1accfef01e842b25fbd0d367a1a5dbc445024c
0MIZI0 <jimin0469@gmail.com>
2022-03-08 09:10:45 +0900

    first commit

---
+++ self/ex01.js
@@ -0,0 +1,12 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+//const -> 상수, unchangable, let -> 변수, changable
+let k = 0;
+k = 1;
+
+//새삥 문법! `` 로 문자열 적기
+const string = `${a} + ${b} + ${c} = ${a+b+c}`;
+console.log(string);
+
